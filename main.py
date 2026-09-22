daf8993f6263e1fc52b67b345b88f8c3da305101
Axykor <sgridley84@gmail.com>
2026-09-22 15:06:22 +0100

    B: got a clock now!

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 100
     
 
 
